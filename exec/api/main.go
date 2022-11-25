d6f0bd2d9eda9a0d9b56994fc2dd00726fe9bbc4
Siyang Zhang <me@siyangz.me>
2022-11-25 13:59:37 +0800

    feat: 更新主程序逻辑。日志、初始化、中间件、pprof
    pprof 监听到了 6060 端口
    设置全局时区到 UTC
    换用了自己的 gin Recovery

--- exec/api/main.go
+++ exec/api/main.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"math/rand"
+	"net/http"
+	_ "net/http/pprof"
+	"os"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -10,6 +15,8 @@ import (
 	"hello/define"
 	"hello/exec/api/controller"
 	"hello/exec/api/middleware"
+	"hello/pkg/gin/log"
+	"hello/pkg/utils"
 	"hello/pkg/zaps"
 )
 
@@ -19,6 +26,7 @@ func setRoute(root *gin.Engine) {
 		mainCtl := controller.CheckController{}
 		gCheck.GET("/machine/info", mainCtl.MachineInfo)
 		gCheck.GET("/ping", mainCtl.Ping)
+		gCheck.GET("/envs", middleware.UserAuth, mainCtl.Envs)
 	}
 	tmp := root.Group("/tmp")
 	{
@@ -29,16 +37,46 @@ func setRoute(root *gin.Engine) {
 }
 
 func getEngine() *gin.Engine {
-	r := gin.Default()
-	r.Use(middleware.HeaderMachine, middleware.SetLogger)
+	r := gin.New()
+	r.Use(middleware.SetLogger)
+	r.Use(log.LoggerWithConfig(gin.LoggerConfig{
+		Formatter: log.LogFormatter,
+		Output:    os.Stdout,
+		SkipPaths: nil,
+	}))
+	r.Use(gin.Recovery()) // fallback
+	r.Use(log.RecoveryWithZaps)
+	r.Use(middleware.HeaderMachine)
 	return r
 }
 
+func Init() {
+	rand.Seed(time.Now().UnixNano())
+	time.Local = time.UTC
+	_ = os.Setenv("TZ", "UTC")
+	middleware.EnableUUIDRandPool()
+
+	zaps.RegisterContextUnboxer(func(ctx context.Context) context.Context {
+		if ginContext, ok := ctx.(*gin.Context); ok {
+			return ginContext.Request.Context()
+		}
+		return nil
+	})
+}
+
 func main() {
+	Init()
 	logger := zaps.GlobalLogger()
-	logger.Info(fmt.Sprintf("start, version %s, build time: %d(%v)", define.BinaryVersion, define.BinaryBuildTime, time.Unix(define.BinaryBuildTime, 0).Format(time.RFC3339)))
+	defer utils.IgnoreErr(logger.Sync)
+
+	go func() {
+		logger.Sugar().Info(http.ListenAndServe("localhost:6060", nil))
+	}()
+
+	logger.Info(fmt.Sprintf("start, version %s, build time: %d(%v)",
+		define.BinaryVersion, define.BinaryBuildTime, time.Unix(define.BinaryBuildTime, 0).Format(time.RFC3339)))
+
 	root := getEngine()
-	gin.DisableConsoleColor()
 
 	setRoute(root)
 
